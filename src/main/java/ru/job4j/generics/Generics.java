089328caa4bae3aecb600a563bd4ebdd79bf91c5
ivansvyatobog <kelthasaugust@yandex.ru>
2023-01-11 12:01:21 +0300

    FlatMap для Iterator<Iterator>

---
+++ src/main/java/ru/job4j/generics/Generics.java
@@ -0,0 +1,63 @@
+package ru.job4j.generics;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Generics {
+    public static void main(String[] args) {
+        Generics gen = new Generics();
+        List<Animal> first = new ArrayList<>();
+        List<Predator> second = new ArrayList<>();
+        List<Tiger> third = new ArrayList<>();
+        first.add(new Animal());
+        second.add(new Predator());
+        third.add(new Tiger());
+
+        gen.printObject(first);
+        gen.printObject(second);
+        gen.printObject(third);
+        System.out.println();
+        /**
+         * Не можем вызвать вызвать метод printBoundedWildCard
+         * т.к метод ограничен сверху классом Predator и поэтому добавление класса Animal незозможно
+         * Добавить можно только классы Predator и его наследника Tiger
+         */
+//        gen.printBoundedWildCard(first);
+//        gen.printBoundedWildCard(second);
+//        gen.printBoundedWildCard(third);
+//        System.out.println();
+
+        /**
+         * Не можем вызвать метод printLowerBoundedWildCard
+         * т.к мотед ограничен снизу классом Predator, добавление объекта класса Tiger невозможно
+         * Добавить можно только классы Predator и его родителя класс Animal
+         */
+
+//        gen.printLowerBoundedWildCard(first);
+//        gen.printLowerBoundedWildCard(second);
+//        gen.printLowerBoundedWildCard(third);
+    }
+
+    public void printObject(List<?> list) {
+        for (Iterator<?> it = list.iterator(); it.hasNext();) {
+            Object next = it.next();
+            System.out.println("Текущий элемент: " + next);
+        }
+    }
+
+
+    public void printBoundedWildCard(List<? extends Predator> list) {
+        for (Iterator<? extends Predator> it = list.iterator(); it.hasNext();) {
+            Object next = it.next();
+            System.out.println("Текущий элемент: " + next);
+        }
+    }
+
+    public void printLowerBoundedWildCard(List<? super Predator> list) {
+        for (Iterator<? super Predator> it = list.iterator(); it.hasNext();) {
+            Object next = it.next();
+            System.out.println("Текущий элемент: " + next);
+        }
+    }
+}
