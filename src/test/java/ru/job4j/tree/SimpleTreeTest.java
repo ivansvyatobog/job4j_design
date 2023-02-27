14f9527d04c09605e1ff40855dcfc5c94757ab3d
ivansvyatobog <kelthasaugust@yandex.ru>
2023-02-27 18:45:22 +0300

    Правка

--- src/test/java/ru/job4j/tree/SimpleTreeTest.java
+++ src/test/java/ru/job4j/tree/SimpleTreeTest.java
@@ -43,4 +43,27 @@ public class SimpleTreeTest {
         tree.add(2, 5);
         assertThat(tree.add(2, 6)).isTrue();
     }
+
+    @Test
+    void whenTreeIsNotBinary() {
+        Tree<Integer> tree = new SimpleTree<>(1);
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(3, 4);
+        tree.add(3, 6);
+        tree.add(3, 7);
+        assertThat(tree.isBinary()).isFalse();
+    }
+
+    @Test
+    void whenTreeIsBinary() {
+        Tree<Integer> tree = new SimpleTree<>(1);
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(2, 4);
+        tree.add(2, 6);
+        tree.add(3, 7);
+        tree.add(3, 8);
+        assertThat(tree.isBinary()).isTrue();
+    }
 }
