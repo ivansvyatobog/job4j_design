d32db9692cf092f208fd15520209b141552d2a6b
ivansvyatobog <kelthasaugust@yandex.ru>
2023-03-10 13:08:53 +0300

    Валидация параметров запуска

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -10,9 +10,7 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        if (args.length == 0) {
-            throw new IllegalArgumentException("The root folder is empty or search argument is null. Use ROOT_FOLDER or SEARCH_ARGUMENT");
-        }
+        validate(args);
         Path start = Paths.get(args[0]);
         search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
@@ -21,5 +19,21 @@ public class Search {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    public static void validate(String[] args) {
+        if (args.length != 2) {
+            throw new IllegalArgumentException("The root folder is empty or search argument is null. Use ROOT_FOLDER or SEARCH_ARGUMENT");
+        }
+        File start = Paths.get(args[0]).toFile();
+        if (!start.exists()) {
+            throw new IllegalArgumentException(String.format("%s - not exist", args[0]));
+        }
+        if (!start.isDirectory()) {
+            throw new IllegalArgumentException(String.format("%s - not directory", args[0]));
+        }
+        if (!args[1].startsWith(".") || args[1].length() < 2) {
+            throw new IllegalArgumentException("This search argument is not correct!");
+        }
     }
 }
