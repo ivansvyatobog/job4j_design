c9d32944ccf71ab8faf810434d3cf3b9fa0514e4
ivansvyatobog <kelthasaugust@yandex.ru>
2023-03-16 13:54:38 +0300

    Правка для корректной работы

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -22,7 +22,7 @@ public class Search {
     }
 
     public static void validate(String[] args) {
-        if (args.length != 2) {
+        if (args.length < 2) {
             throw new IllegalArgumentException("The root folder is empty or search argument is null. Use ROOT_FOLDER or SEARCH_ARGUMENT");
         }
         File start = Paths.get(args[0]).toFile();
