6d4cfecd6eef97f5bd8a9dc6bdba9673544181c9
ivansvyatobog <kelthasaugust@yandex.ru>
2023-03-31 17:12:51 +0300

    3. Бот [#7921]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -8,17 +8,19 @@ public class EchoServer {
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
-                Socket socket = server.accept();
-                try (OutputStream out = socket.getOutputStream();
+                Socket client = server.accept();
+                try (OutputStream out = client.getOutputStream();
                      BufferedReader in = new BufferedReader(
-                             new InputStreamReader(socket.getInputStream()))) {
+                             new InputStreamReader(client.getInputStream()))) {
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                     String str = in.readLine();
-                    if (str.contains("Bye")) {
+                    if (str.contains("Hello")) {
+                        out.write("Hello".getBytes());
+                    }else if (str.contains("Exit")) {
+                        out.write("Работа сервера завершена!".getBytes());
                         server.close();
-                    }
-                    for (str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
-                        System.out.println(str);
+                    } else {
+                        out.write("What".getBytes());
                     }
                     out.flush();
                 }
