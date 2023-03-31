8b32a52b9126187bd953e044a71b8fcb7f7acd2e
ivansvyatobog <kelthasaugust@yandex.ru>
2023-03-31 15:28:03 +0300

    CSVReader edit

--- src/main/java/ru/job4j/io/CSVReader.java
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -10,21 +10,14 @@ public class CSVReader {
     public static void handle(ArgsName argsName) {
         validate(argsName);
         String output = argsName.get("out");
-        try (Scanner scanner = new Scanner(new FileInputStream(argsName.get("path")));
-             PrintStream out = "stdout".equals(output) ? new PrintStream(System.out) :
-                     new PrintStream(new FileOutputStream(output))
-        ) {
+        try (Scanner scanner = new Scanner(new FileInputStream(argsName.get("path"))); PrintStream out = "stdout".equals(output) ? new PrintStream(System.out) : new PrintStream(new FileOutputStream(output))) {
             scanner.useDelimiter(System.lineSeparator());
             List<String> filters = Arrays.stream(argsName.get("filter").split(",")).toList();
             List<String> elementsList = Arrays.stream(scanner.next().split(argsName.get("delimiter"))).toList();
-            List<Integer> elementIndex = filters.stream()
-                    .mapToInt(elementsList::indexOf)
-                    .filter(i -> i != -1)
-                    .boxed()
-                    .toList();
+            List<Integer> elementIndex = filters.stream().mapToInt(elementsList::indexOf).filter(i -> i != -1).boxed().toList();
             String filteredElements = filter(elementsList, elementIndex, argsName.get("delimiter"));
             out.println(filteredElements);
-            while(scanner.hasNext()) {
+            while (scanner.hasNext()) {
                 List<String> line = Arrays.asList(scanner.next().split(argsName.get("delimiter")));
                 String filteredLine = filter(line, elementIndex, argsName.get("delimiter"));
                 out.println(filteredLine);
@@ -36,23 +29,18 @@ public class CSVReader {
     }
 
     private static String filter(List<String> listOfElements, List<Integer> elementIndexes, String delimiter) {
-        return elementIndexes.stream()
-                .map(listOfElements::get)
-                .collect(Collectors.joining(delimiter));
+        return elementIndexes.stream().map(listOfElements::get).collect(Collectors.joining(delimiter));
     }
 
     private static void validate(ArgsName argsName) {
         if (!Files.exists(Paths.get(argsName.get("path"))) || !argsName.get("path").endsWith(".csv")) {
-            throw new IllegalArgumentException(
-                    String.format("%s - not exist or file format is not correct", argsName.get("path")));
+            throw new IllegalArgumentException(String.format("%s - not exist or file format is not correct", argsName.get("path")));
         }
         if (argsName.get("out").isEmpty() || !argsName.get("out").endsWith(".csv")) {
-            throw new IllegalArgumentException(
-                    String.format("%s - not exist or file format is not correct", argsName.get("out")));
+            throw new IllegalArgumentException(String.format("%s - not exist or file format is not correct", argsName.get("out")));
         }
         if (argsName.get("filter").isBlank()) {
-            throw new IllegalArgumentException(
-                    String.format("%s - filter argument is empty!", argsName.get("filter")));
+            throw new IllegalArgumentException(String.format("%s - filter argument is empty!", argsName.get("filter")));
         }
     }
 
