b341ca69a240f0269a4ce8885f03f3db16f66a3f
ivansvyatobog <kelthasaugust@yandex.ru>
2023-04-07 11:32:59 +0300

    0. Что такое база данных. [#6860]

---
+++ data.sql
@@ -0,0 +1,16 @@
+create table car_models(
+	id serial primary key,
+	name varchar(255),
+	model text,
+	manufactured date
+);
+
+insert into car_models(name, model, manufactured) values ('Ваз', 'Priora', '23.08.2016');
+
+select * from car_models;
+
+update car_models set name = 'Vaz';
+
+delete from car_models;
+
+select * from car_models;
